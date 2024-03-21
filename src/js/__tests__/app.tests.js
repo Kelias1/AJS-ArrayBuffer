9b3a84b34f90c009a68d158e2162ba387cdc888e
Kelias1 <kelsito24@gmail.com>
2024-03-21 12:55:49 +0300

    first commit

---
+++ src/js/__tests__/app.tests.js
@@ -0,0 +1,12 @@
+import getBuffer from '../app';
+import ArrayBufferConverter from '../ArrayBufferConverter';
+
+test('convert ArrayBuffer to string', () => {
+  const buffer = getBuffer();
+  const convert = new ArrayBufferConverter();
+  convert.load(buffer);
+
+  const result = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+
+  expect(convert.toString()).toEqual(result);
+});
